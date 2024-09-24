966a06de35a895a94ca95690118c509eb5d4e554
Paul Paczuski <6165713+plpxsk@users.noreply.github.com>
2024-09-24 11:14:02 -0400

    Done: losses going down during training #win
    Uses python script

--- qa.py
+++ qa.py
@@ -1,12 +1,105 @@
 from functools import partial
+import numpy as np
+import time
+import argparse
+import math
+import logging
+
 import mlx.core as mx
 import mlx.nn as nn
+import mlx.optimizers as optim
+
+from utils import load_processed_datasets
+
+
+def main(args):
+    model, tokenizer = load_model_tokenizer(hf_model=args.model_str,
+                                            mlx_weights_path=args.mlx_weights)
+
+    train_ds, valid_ds, test_ds = load_processed_datasets(
+        filter_size=args.dataset_size, model_max_length=tokenizer.model_max_length,
+        tokenizer=tokenizer)
+
+    # set logger after loading squad
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.log)
+    logging.basicConfig(level=numeric_level)
+
+    mx.eval(model.parameters())
+    optimizer = optim.AdamW(learning_rate=1e-5)
+    state = [model.state, optimizer.state]
+
+    @partial(mx.compile, inputs=state, outputs=state)
+    def step(input_ids, token_type_ids, attention_mask, start_positions,
+             end_positions):
+        loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+        loss, grads = loss_and_grad_fn(
+            model, input_ids, token_type_ids, attention_mask, start_positions,
+            end_positions)
+        optimizer.update(model, grads)
+        return loss
+
+    train_iterator = batch_iterate(train_ds, batch_size=args.batch_size)
+    losses = []
+    tic = time.perf_counter()
+
+    for it, batch in zip(range(args.num_iters), train_iterator):
+        logging.info(f"Iteration {it}...")
+        input_ids, token_type_ids, attention_mask, start_positions, end_positions = map(
+            mx.array,
+            (batch['input_ids'], batch['token_type_ids'], batch['attention_mask'],
+             batch['start_positions'], batch['end_positions'])
+        )
+
+        loss = step(input_ids, token_type_ids, attention_mask,
+                    start_positions, end_positions)
+
+        mx.eval(state)
+        losses.append(loss.item())
+        if (it + 1) % args.steps_per_report == 0:
+            logging.info("Running report...")
+            train_loss = np.mean(losses)
+            toc = time.perf_counter()
+            print(
+                f"Iter {it + 1}: Train loss {train_loss:.3f}, "
+                f"It/sec {args.steps_per_report / (toc - tic):.3f}"
+            )
+            losses = []
+            tic = time.perf_counter()
+        if (it + 1) % args.steps_per_eval == 0:
+            logging.info("Checking val_loss...")
+            val_loss = eval_fn(valid_ds, model, batch_size=args.batch_size)
+            toc = time.perf_counter()
+            print(
+                f"Iter {it + 1}: "
+                f"Val loss {val_loss:.3f}, "
+                f"Val ppl {math.exp(val_loss):.3f}, "
+                f"Val took {(toc - tic):.3f}s, "
+            )
+            tic = time.perf_counter()
+
+    # TODO
+    # Check test score at end of training
+
+    # if args.train:
+    #     opt = opt
+    #     train()
+    #     mx.savez(save_file)
+
+    # if args.test:
+    #     model.eval()
+    #     test_loss = test_loss
+    #     test_ppl = test_ppl
+
+    #     return test_loss, test_ppl
+
+    # if args.inference:
+    #     context, question = context, question
+    #     run()
 
 
 def batch_iterate(dataset, batch_size):
-    # try to get rid of this dep
-    import numpy as np
-
     perm = np.random.default_rng(12345).permutation(len(dataset))
     # # do not use this??
     # # it won't work at least in juypyter because need ids[iter].item()
@@ -46,7 +139,12 @@ def eval_fn(dataset, model, batch_size=8):
         )
         losses = loss_fn(model, input_ids, token_type_ids, attention_mask,
                          start_positions, end_positions, reduce=False)
+        losses_have_nans = mx.isnan(losses).any()
+        if losses_have_nans:
+            logging.debug(f"eval_fn() found losses with nans: {losses}")
         loss += mx.sum(losses).item()
+    logging.debug(f"eval_fn() final loss: {loss}")
+    logging.debug(f"eval_fn() len(dataset): {len(dataset)}")
     return loss / len(dataset)
 
 
@@ -63,158 +161,56 @@ def load_model_tokenizer(hf_model: str, mlx_weights_path: str = "weights/bert-ba
     return model, tokenizer
 
 
-def main():
-    import mlx.optimizers as optim
-    from utils import load_processed_datasets
-
-    print("train or inference using this script")
-    print("Follow SETUP IN mlx-examples/lora")
-
-    bert_model = "bert-base-uncased"
-    mlx_weights_path = "weights/bert-base-uncased.npz"
-    model, tokenizer = load_model_tokenizer(hf_model=bert_model,
-                                            mlx_weights_path=mlx_weights_path)
-
-    train_ds, valid_ds, test_ds = load_processed_datasets(
-        filter_size=100, model_max_length=tokenizer.model_max_length,
-        tokenizer=tokenizer)
-
-    @partial(mx.compile, inputs=state, outputs=state)
-    def step(input_ids, token_type_ids, attention_mask, start_positions, end_positions):
-        loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
-        loss, grads = loss_and_grad_fn(
-            model, input_ids, token_type_ids, attention_mask, start_positions, end_positions)
-        optimizer.update(model, grads)
-    return loss
-
-    optimizer = optim.AdamW(learning_rate=1e-5)
-
-    mx.eval(model.parameters())
-    loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
-
-    for epoch in range(n_epoch):
-        print(f"Training for epoch {epoch+1}...")
-        # model.train()
-        # for batch in train_dataloader:
-
-        for batch in batch_iterate(train_ds, batch_size=16):
-            print(len(batch['input_ids']))
-
-            batch = next(iter(batch_iterate(train_ds, batch_size=16)))
-
-            # input_ids = batch['input_ids'].to(device)
-            # attention_mask = batch['attention_mask'].to(device)
-            # start_positions = batch['start_positions'].to(device)
-            # end_positions = batch['end_positions'].to(device)
-
-            # TODO: mx.array() in preprocess_tokenize_function() ????
-            input_ids = mx.array(batch['input_ids'])
-            # input_ids = mx.expand_dims(input_ids, 0)
-
-            token_type_ids = mx.array(batch['token_type_ids'])
-            # token_type_ids = mx.expand_dims(token_type_ids, 0)
-
-            attention_mask = mx.array(batch['attention_mask'])
-            # attention_mask = mx.expand_dims(attention_mask, 0)
-
-            start_positions = mx.array(batch['start_positions'])
-
-            end_positions = mx.array(batch['end_positions'])
-
-            outputs, start_logits, end_logits = model(input_ids=input_ids,
-                                                      token_type_ids=token_type_ids,
-                                                      attention_mask=attention_mask,
-                                                      start_positions=start_positions,
-                                                      end_positions=end_positions)
-
-            # NEXT: continue here
-            # NEXT: MODULARIZE so I can use in Notebook
-
-            # use ipynb notebook for outputs
-
-            start_loss = loss_fn(start_logits, start_positions)
-            end_loss = loss_fn(end_logits, end_positions)
-            total_loss = (start_loss + end_loss) / 2
-            # # TODO
-            loss, _ = loss_and_grad_fn(
-                start_logits, end_logits, start_positions, end_positions)
-            # end_loss, _ = loss_and_grad_fn(end_logits, end_positions)
-
-            # loss from nn.value_and_grad() or similar??
-            loss = outputs["loss"]
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        # Evaluate the model
-        model.eval()
-        total_valid_loss = 0
-        for batch in valid_dataloader:
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            start_positions = batch['start_positions'].to(device)
-            end_positions = batch['end_positions'].to(device)
-
-            with torch.no_grad():
-                outputs = model(input_ids, attention_mask=attention_mask,
-                                start_positions=start_positions, end_positions=end_positions)
-                loss = outputs["loss"]
-
-            total_valid_loss += loss.item()
-
-        avg_valid_loss = total_valid_loss / len(valid_dataloader)
-        print(f'Epoch: {epoch+1}, Valid Loss: {avg_valid_loss}')
-
-    # Now, check TEST score
-    total_test_loss = 0
-    for batch in test_dataloader:
-        print(f"Running batch...")
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        start_positions = batch['start_positions'].to(device)
-        end_positions = batch['end_positions'].to(device)
-
-        with torch.no_grad():
-            outputs = model(input_ids, attention_mask=attention_mask,
-                            start_positions=start_positions, end_positions=end_positions)
-            loss = outputs["loss"]
-
-        total_test_loss += loss.item()
-
-    avg_test_loss = total_test_loss / len(test_dataloader)
-    print(f'Final Test Loss: {avg_test_loss}')
-
-    if args.train:
-        opt = opt
-        train()
-        mx.savez(save_file)
-
-    if args.test:
-        model.eval()
-        test_loss = test_loss
-        test_ppl = test_ppl
-
-        return test_loss, test_ppl
-
-    if args.inference:
-        context, question = context, question
-        run()
-
-
 def build_parser():
     parser = argparse.ArgumentParser(description="TBD")
     parser.add_argument(
-        "--model",
-        default="mlx_model",
-        help="The path to the local model directory or Hugging Face repo.",
+        "--model_str",
+        default="bert-base-uncased",
+        help="Name of BERT model for tokenizer and parameters",
+    )
+    parser.add_argument(
+        "--mlx_weights",
+        default="weights/bert-base-uncased.npz",
+        help="The path to the local MLX model weights",
     )
     parser.add_argument(
         "--train",
         action="store_true",
         help="Do training",
     )
+    parser.add_argument("--dataset_size", type=int, default=1000,
+                        help="Number of records to load for entire dataset.")
+    parser.add_argument("--batch_size", type=int, default=2, help="Minibatch size.")
+    parser.add_argument(
+        "--num_iters", type=int, default=4, help="Iterations to train for."
+    )
+    parser.add_argument(
+        "--steps_per_report",
+        type=int,
+        default=2,
+        help="Number of training steps between loss reporting.",
+    )
+    parser.add_argument(
+        "--steps_per_eval",
+        type=int,
+        default=3,
+        help="Number of training steps between validations.",
+    )
+    parser.add_argument(
+        "--eval_test",
+        action="store_true",
+        help="Evaluate on the test set after training",
+    )
+    parser.add_argument(
+        "--log",
+        default="warning",
+        help="Set python logging level from: warn (default)"
+    )
+    return parser
 
 
 if __name__ == '__main__':
-    main()
+    parser = build_parser()
+    args = parser.parse_args()
+
+    main(args)
