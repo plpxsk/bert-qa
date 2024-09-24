62f8373a279ca318efb00c7fcf9840f264d3a353
Paul Paczuski <6165713+plpxsk@users.noreply.github.com>
2024-09-24 12:35:48 -0400

    Take out the garbage

--- model_mlx.py
+++ model_mlx.py
@@ -167,32 +167,6 @@ class BertQA(nn.Module):
         return start_logits, end_logits
 
 
-def tmp():
-    def tmp():
-        # NEXT: take this into qa.py with loss_fn()
-        total_loss = None
-        if start_positions is not None and end_positions is not None:
-            # # If we are on multi-GPU, split add a dimension
-            # if len(start_positions.size()) > 1:
-            #     start_positions = start_positions.squeeze(-1)
-            # if len(end_positions.size()) > 1:
-            #     end_positions = end_positions.squeeze(-1)
-
-            # # sometimes the start/end positions are outside our model inputs, we ignore these terms
-            # ignored_index = start_logits.size(1)
-            # start_positions = start_positions.clamp(0, ignored_index)
-            # end_positions = end_positions.clamp(0, ignored_index)
-
-            loss_fn = nn.losses.cross_entropy()
-            start_loss = loss_fn(start_logits, start_positions)
-            end_loss = loss_fn(end_logits, end_positions)
-            total_loss = (start_loss + end_loss) / 2
-
-        # # TODO check argument 2:
-        # output = (start_logits, end_logits) + outputs[2:]
-        # return ((total_loss,) + output)
-
-
 def load_model(
     bert_model: str, weights_path: str
 ) -> Tuple[Bert, PreTrainedTokenizerBase]:
